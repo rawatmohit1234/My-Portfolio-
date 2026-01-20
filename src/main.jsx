13f6ba2c588a0b9da2249e0d3600acc76df557bc
rawatmohit1234 <mdhirawat2004@gmail.com>
2026-01-20 16:59:05 +0530

    Add netlify.toml for SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
-  <App />
-
-)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
